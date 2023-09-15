3fcc57145eebb055d520ee38ac72f59e8dcbf10e
Jordan Denison <jordan@denisonweb.com>
2023-09-15 20:15:47 +0000

    impl capitalizeAllFirstLetters

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export const capitalizeFirstLetter = (str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export const capitalizeAllFirstLetters = (str: string): string => {
+  return str.split(' ').map(capitalizeFirstLetter).join(' ')
+}
---
+++ tests/utils.capitalize_all_first_letters.test.ts
@@ -0,0 +1,31 @@
+import { capitalizeAllFirstLetters } from "../src"
+
+test('capitalizes first letter of each word', async () => {
+  const result = capitalizeAllFirstLetters('hello world')
+
+  expect(result).toBe('Hello World')
+})
+
+test('does not affect already capitalized letters', async () => {
+  const result = capitalizeAllFirstLetters('Hello World')
+
+  expect(result).toBe('Hello World')
+})
+
+test('handles single word input correctly', async () => {
+  const result = capitalizeAllFirstLetters('hello')
+
+  expect(result).toBe('Hello')
+})
+
+test('does not trim trailing whitespace', async () => {
+  const result = capitalizeAllFirstLetters('hello  ')
+
+  expect(result).toBe('Hello  ')
+})
+
+test('handles empty string input correctly', async () => {
+  const result = capitalizeAllFirstLetters('')
+
+  expect(result).toBe('')
+})
